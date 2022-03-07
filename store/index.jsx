a304953df4b359f43fdea0b7f997ac7d6ea4a4b4
Sarah <sarah.bao111@gmail.com>
2022-03-07 17:16:45 +0100

    add redux-persit to save liked jobs locally

--- store/index.jsx
+++ store/index.jsx
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import thunk  from 'redux-thunk';
 import reducers from '../reducers';
 
@@ -6,8 +8,9 @@ const store = createStore(
     reducers,
     {},
     compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    autoRehydrate()
     )
 );
-
+persistStore(store, {storage: AsyncStorage, whitelist: ['likedjobs']})
 export default store;
